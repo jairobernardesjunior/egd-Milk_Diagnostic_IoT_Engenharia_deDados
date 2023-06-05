5d861ef743612f202b88917a909a9d3950622ec3
unknown <jairobernardesjuniorr@gmail.com>
2023-06-05 15:56:29 -0300

    v2.3
    v2.3

--- milk py/get_milk_json.py
+++ milk py/get_milk_json.py
@@ -15,6 +15,32 @@ O sistema fornece uma senha que você precisa usar para autenticar no python.
 import imaplib
 import email
 import yaml 
+import datetime
+import pandas as pd
+from fc import fc_upload_s3 as ups3
+
+def grava_sobes3_arquivo_json_lapidado(
+        dirAux, nome_arquivo, df, s3_dados_processed, access_key, secret_key, regiao):
+
+    nome_arquivo = nome_arquivo + '.json'
+    pathJson = dirAux + '/' + nome_arquivo
+
+    df.to_json(pathJson)
+
+    return True
+
+    """
+
+    # ******************** CARREGA ARQUIVO json NO BUCKET S3
+    retorno = ups3.upload_s3(
+            s3_dados_processed, nome_arquivo, pathJson, access_key, secret_key, regiao)
+
+    if retorno != True:
+        print(retorno)
+        print('bucket s3 => ' + s3_dados_processed + ' arquivo => ' + nome_arquivo + 
+                ' --- ' + retorno + ' ***** não foi carregado')    
+
+    return retorno """
 
 def lambda_handler(event, context):
 # ******************** INÍCIO
@@ -27,7 +53,9 @@ def lambda_handler(event, context):
 
     # separa parâmetros em suas variáveis
     user, password = my_credentials["user"], my_credentials["password"]
-    imap_url = my_credentials["url_imap"]
+    access_key, secret_key = my_credentials["access_key"], my_credentials["secret_key"]
+    region, s3_dados_processed = my_credentials["region"], my_credentials["mk-s3-milk-json"]
+    imap_url, dirAux = my_credentials["url_imap"], my_credentials["dirAux"]
 
     # conecta gmail
     my_mail = imaplib.IMAP4_SSL(imap_url)
@@ -47,6 +75,11 @@ def lambda_handler(event, context):
     mail_id_list = data[0].split()
     msgs = []
 
+    # carrega msgs
+    for num in mail_id_list:
+        typ, data = my_mail.fetch(num, '(RFC822)') #RFC822 returns whole message (BODY fetches just body)
+        msgs.append(data)
+
     # Separa campos do email e monta documento json
     cod_produtor=[]
     datax=[]
@@ -62,13 +95,25 @@ def lambda_handler(event, context):
     tleite5=[]
     tleite6=[]
     tleite7=[]  
-    tleite8=[]  
+    tleite8=[] 
 
-    # carrega msgs
-    for num in mail_id_list:
-        typ, data = my_mail.fetch(num, '(RFC822)') #RFC822 returns whole message (BODY fetches just body)
-        msgs.append(data)
+    cod_produtor.clear
+    datax.clear
+    horax.clear
+    lat.clear
+    long.clear
+    umidade.clear
+    tempex.clear
+    tleite1.clear
+    tleite2.clear
+    tleite3.clear
+    tleite4.clear
+    tleite5.clear
+    tleite6.clear
+    tleite7.clear
+    tleite8.clear     
 
+    ii=0
     for msg in msgs[::-1]:
         for response_part in msg:
             if type(response_part) is tuple:
@@ -87,9 +132,9 @@ def lambda_handler(event, context):
 
                         #print(body_aux)
                         #print(body_aux[0:2])
-                        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")                     
-
-                        if body_aux[0:2] == 'r1':                            
+                        #print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")     
+                               
+                        if body_aux[0:2] == 'r1':                             
                             i=0
                             while i<15:
                                 pos=body_aux.find('p') + 1
@@ -98,7 +143,7 @@ def lambda_handler(event, context):
                                 pos=body_aux.find('p')
                                 campo= body_aux[0:pos]
 
-                                print(campo)
+                                #print(campo)
 
                                 match i:
                                     case 0:
@@ -132,20 +177,53 @@ def lambda_handler(event, context):
                                     case 14:
                                         tleite8.append(campo)
 
-                                i+=1                                                                                                                                                                                                                                          
+                                i+=1 
+                                ii+=1  
 
-                        mail_ids = []
+    if ii>0:
+        df=pd.DataFrame({
+                "cod_produtor":cod_produtor,
+                "datax":datax,
+                "horax":horax,
+                "lat":lat,
+                "long":long,
+                "umidade":umidade,
+                "tempex":tempex,
+                "tleite1":tleite1,
+                "tleite2":tleite2,
+                "tleite3":tleite3,
+                "tleite4":tleite4,
+                "tleite5":tleite5,
+                "tleite6":tleite6,
+                "tleite7":tleite7,
+                "tleite8":tleite8,                         
+                })
 
-                        for block in search_data:
-                            mail_ids += block.split()
+        nome_arquivo = 'milk_' + str(datetime.datetime.now())
+        nome_arquivo = nome_arquivo.replace(' ', '_')
+        nome_arquivo = nome_arquivo.replace(':', '')
+        nome_arquivo = nome_arquivo.replace('.', '_')
+        retorno = grava_sobes3_arquivo_json_lapidado(
+                    dirAux, nome_arquivo, df, s3_dados_processed, 
+                    access_key, secret_key, region)
 
-                        # definindo o range da operação
-                        start = mail_ids[0].decode()
-                        end = mail_ids[-1].decode()
+        if retorno == True:
+            mail_ids = []
 
-                        # movendo os emails para a lixeira
-                        # este passo é específico do gmail
-                        # que não permite a exclusão direta
-                        #my_mail.store(f'{start}:{end}'.encode(), '+X-GM-LABELS', '\\Trash')                                   
+            for block in search_data:
+                mail_ids += block.split()
+
+            # definindo o range da operação
+            start = mail_ids[0].decode()
+            end = mail_ids[-1].decode()
+
+            # movendo os emails para a lixeira
+            # este passo é específico do gmail
+            # que não permite a exclusão direta
+            my_mail.store(f'{start}:{end}'.encode(), '+X-GM-LABELS', '\\Trash')                    
+
+    else:
+        print("+++++ Email sem dados")
+        print (my_msg)                
 
 lambda_handler(1, 1)                            
